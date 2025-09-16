aafa6b0493873a1793bb51a8972d774a58ba40a8
ramazanismayilov <ramazanismayilovh@gmail.com>
2025-09-16 09:59:08 +0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,86 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { JwtModule } from '@nestjs/jwt';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ClsModule } from 'nestjs-cls';
+import { Request } from 'express';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          url: config.get('DATABASE_URL'),
+          entities: [join(__dirname, '/entities/*.entity.{ts,js}')],
+          migrations: [join(__dirname, '/migrations/*.{ts,js}')],
+          logging: true,
+          synchronize: true,
+        };
+      },
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory(config: ConfigService) {
+        return {
+          secret: config.get('JWT_SECRET'),
+          signOptions: { expiresIn: '1d' },
+        };
+      },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    ClsModule.forRoot({
+      global: true,
+      middleware: {
+        mount: true,
+        setup: (cls, req: Request) => {
+          cls.set('ip', req.ip);
+        },
+      },
+    }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory(config: ConfigService) {
+        return {
+          transport: {
+            service: 'gmail',
+            host: config.get('SMTP_HOST'),
+            port: config.get('SMTP_PORT'),
+            secure: config.get('SMTP_SECURE'),
+            auth: {
+              user: config.get('SMTP_USER'),
+              pass: config.get('SMTP_PASSWORD'),
+            },
+          },
+          defaults: {
+            from: `"166 Cargo" <${config.get('SMTP_FROM')}>`,
+          },
+          template: {
+            dir: join(__dirname, '..', 'src', 'templates'),
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api')
+  app.enableCors()
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Epic Games')
+    .setDescription('The Epic Games API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true
+    }
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
